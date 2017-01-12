a6cc8d3bbb7dd0fabc877463d3e786e4a0cf4aec
Sean Felderman <sean@Seans-MacBook-Pro-3.local>
2017-01-11 23:28:22 -0800

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp        = require('gulp');
+var sass = require('gulp-sass');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass().on('error', function (err) {
+	    console.log(err);
+	    this.emit('end');
+    })) // Passes it through a gulp-sass
+    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync'], 'watch',
+    callback
+  )
+})
